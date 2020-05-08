cb74f69a94ea4560f64f92fcb721f5cc9bc5dc0b
Erik <erikolivesjo@gmail.com>
2020-05-08 13:49:17 +1000

    added test for Vector

---
+++ Test_Vector.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+
+#include "Vector.h"
+#include "Tile.h"
+
+int main(void) {
+
+    Vector *testVec = new Vector;
+
+    testVec->addTile(new Tile(RED));
+
+    if(testVec->get(0) == nullptr) {
+        std::cout << "tile is being a fucking fuckhead" << std::endl;
+    } else {
+        std::cout << testVec->get(0)->getColourByChar() << std::endl;
+    }
+
+    testVec->removeTile(0);
+    testVec->addTile(new Tile(BLACK));
+    testVec->addTile(new Tile(DARK_BLUE));
+
+    if(testVec->get(0) == nullptr) {
+        std::cout << "tile is being a fucking fuckhead" << std::endl;
+    } else {
+        std::cout << testVec->get(0)->getColourByChar() << std::endl;
+    }
+
+    std::cout << testVec->get(0)->getColourByChar() << std::endl;
+    std::cout << testVec->get(1)->getColourByChar() << std::endl;
+    
+    delete testVec;
+
+    return EXIT_SUCCESS;
+}
